91b0862a1df09400eeb15a02a59a8105f489cf44
jctiru <jonathanchristiantiru@gmail.com>
2019-08-14 15:19:20 +0800

    Create initial page

---
+++ spring-mvc-sandbox/src/test/springmvcsandbox/springdemo/mvc/HomeController.java
@@ -0,0 +1,14 @@
+package test.springmvcsandbox.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+	
+}
